0887e794e7ee961f4c85be31623f7b343d376e37
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-15 23:53:08 +0200

    now the Flyweight as comparison, Singleton only has now instance whereas Flyweight - Potentially many instances — but shared and reused based on some key or intrinsic state.

---
+++ src/main/java/pl/piwowarski/SingletonPatternComparison/SingletonExample/TreeFactory.java
@@ -0,0 +1,32 @@
+package pl.piwowarski.SingletonPatternComparison.SingletonExample;
+
+// ---------------------------
+// FLYWEIGHT PATTERN
+// ---------------------------
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TreeFactory {
+    private static final Map<String, TreeModel> treeModels = new HashMap<>();
+
+    public static TreeModel getTreeModel(String type) {
+        treeModels.putIfAbsent(type, createTreeModel(type));
+        return treeModels.get(type);
+    }
+
+    private static TreeModel createTreeModel(String type) {
+        switch (type) {
+            case "Oak":
+                return new ConcreteTreeModel("OakTexture.png", "OakMesh.obj");
+            case "Pine":
+                return new ConcreteTreeModel("PineTexture.png", "PineMesh.obj");
+            default:
+                return new ConcreteTreeModel("DefaultTexture.png", "DefaultMesh.obj");
+        }
+    }
+
+    public static int getModelCount() {
+        return treeModels.size();
+    }
+}
