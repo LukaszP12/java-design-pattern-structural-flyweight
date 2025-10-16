4a82841caf7076d1eeca30dc5af0e2969f309f72
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-16 10:43:39 +0200

    created an example for Factory pattern, to later compare it with Flyweight. a comparison needs to be illustrative and comprehensive

---
+++ src/main/java/pl/piwowarski/FactoryPatternComparision/FactoryvsFlyweightExample/FactoryPatternExample/FactoryDemo.java
@@ -0,0 +1,17 @@
+package pl.piwowarski.FactoryPatternComparision.FactoryvsFlyweightExample.FactoryPatternExample;
+
+public class FactoryDemo {
+    public static void main(String[] args) {
+        System.out.println("=== FACTORY PATTERN ===");
+        Tree t1 = TreeFactory.createTree("Oak");
+        Tree t2 = TreeFactory.createTree("Oak");
+        Tree t3 = TreeFactory.createTree("Pine");
+
+        t1.render(10, 20);
+        t2.render(15, 25);
+        t3.render(50,60);
+
+        System.out.println("t1 == t2 ? " + (t1 == t2));
+        System.out.println("t1.equals(t2) ? " + (t1.equals(t2)));
+    }
+}
---
+++ src/main/java/pl/piwowarski/FactoryPatternComparision/FactoryvsFlyweightExample/FactoryPatternExample/Tree.java
@@ -0,0 +1,5 @@
+package pl.piwowarski.FactoryPatternComparision.FactoryvsFlyweightExample.FactoryPatternExample;
+
+interface Tree {
+    void render(int x, int y);
+}
